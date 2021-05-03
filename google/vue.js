1cf5fa4cdfa2dc490005cbe9ad59f6158df1537b
PiotrKupis999 <piotr.kupis999@gmail.com>
2021-05-04 01:51:03 +0200

    Index.html initial commit

---
+++ google/vue.js
@@ -0,0 +1,24 @@
+var app = new Vue({
+    el: "#app",
+    data: {
+        googleSearch: "",
+        cities: window.cities,
+        isActive: 0,
+        control: 0
+    },
+
+    computed: {
+        filteredCities: function () {
+            if (this.googleSearch.length > 0) {
+                let miasta = this.cities.filter(city => city.name.includes(this.googleSearch))
+                if (miasta.length > 10) {
+                    return miasta.slice(0, 10);
+                }
+                else {
+                    return miasta;
+                }
+            }
+        }
+    },
+
+});
