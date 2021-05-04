8410c26c0fcdee57bdb0d9084753f7788271b02a
PiotrKupis999 <piotr.kupis999@gmail.com>
2021-05-04 03:46:08 +0200

    Index.html initial commit

--- google/vue.js
+++ google/vue.js
@@ -6,7 +6,20 @@ var app = new Vue({
         isActive: 0,
         control: 0
     },
+    updated() {
+        this.$nextTick(() => {
 
+            if (this.change) {
+                this.$refs.dla_results.focus();
+
+            } else {
+
+                this.$refs.dla_start.focus();
+
+            }
+
+        });
+    },
     computed: {
         filteredCities: function () {
             if (this.googleSearch.length > 0) {
@@ -18,6 +31,27 @@ var app = new Vue({
                     return miasta;
                 }
             }
+        }
+    },
+    methods: {
+        autocomplete(city) {
+            this.googleSearch = city.name;
+            this.results = this.googleSearch;
+        },
+
+        wytluszczenie: function(a)
+        {
+            wyszukaj = this.googleSearch;
+            var pom = a.split(wyszukaj);
+            for(i = 0; i < pom.length; i++)
+            {
+                a = a.replace(pom[i], pom[i].bold());
+            }
+            return a;
+        },
+        czy_jest: function()
+        {
+            this.isActive = 1;
         }
     },
 
